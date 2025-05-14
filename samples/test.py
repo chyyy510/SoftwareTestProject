cb369e04f70b9e33c18594456834f000a9873dfa
rea1269 <hulinmin3@gmail.com>
2025-05-14 17:26:29 +0800

    Update test.py

--- samples/test.py
+++ samples/test.py
@@ -8,8 +8,7 @@ def add(a, b):
 
 def multiply(x, y=2):
     return x * y
-<<<<<<< HEAD
-    
+
 def relu(x):
   ''' relu '''
   output=np.maximum(0,x)
@@ -19,10 +18,5 @@ def relu_prime(x):
     ''' relu导数 '''
     bottom_diff=np.where(x > 0, 1, 0)
     return bottom_diff
-=======
 
 
-def relu(x):
-    output = np.maximum(0, x)
-    return output
->>>>>>> 3943beb6c447e68fee05a79b43f42414803962c4
