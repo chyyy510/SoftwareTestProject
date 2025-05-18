7435edc47d460387467dabf986533333ca2f0c39
chyyy510 <kmsmchyyy510@gmail.com>
2025-05-18 21:48:14 +0800

    requirements更新，+sample

---
+++ samples/123.py
@@ -0,0 +1,17 @@
+def bubble_sort(nums):
+    swap = True
+    end = len(nums)
+    while swap == True:
+        swap = False
+        for i in range(1, end):
+            if nums[i - 1] > nums[i]:
+                temp = nums[i - 1]
+                nums[i - 1] = nums[i]
+                nums[i] = temp
+                swap = True
+        end -= 1
+    return nums
+
+
+def test(x):
+    return x * x
